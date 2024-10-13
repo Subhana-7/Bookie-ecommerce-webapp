5766047eb1c23f86b9a2cf04f09c32feb01e0e3d
Subhana <subhanasn2005@gmail.com>
2024-10-13 21:57:39 +0530

    changes of today

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const env = require("dotenv").config(); //here something wrong
 const db = require("./config/db");
+const userRouter = require("./routes/userRouter");
+const adminRouter = require("./routes/adminRouter");
 db();
 
-app.listen(process.env.PORT,() => {
+app.use(express.json()); //parse incoming requests 
+app.use(express.urlencoded({extended:true})); //parse url queries
+
+app.set("view engine","ejs");
+app.set("views",[path.join(__dirname,"views/user"),path.join(__dirname,"views/admin")])
+app.use(express.static(path.join(__dirname,"public"))); //fetch the data in the public folder
+
+
+app.use("/", userRouter);
+
+PORT = 7000 || process.env.PORT
+app.listen(PORT,() => {
   console.log("server running");
 })
 
